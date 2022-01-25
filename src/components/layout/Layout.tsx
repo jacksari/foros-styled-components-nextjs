89ebc51aea64f92633f8e1b98a3ebef41c8dd68d
Jack Sari <janasarii@gmail.com>
2022-01-25 17:21:01 -0500

    add _document.js

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactChildren } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Head from 'next/head';
 
 interface AuxProps  {
   children: React.ReactNode | ReactChildren
@@ -10,13 +9,6 @@ interface AuxProps  {
 const Layout = ({ children }: AuxProps) => {
   return (
     <>
-        <Head>
-
-            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600&display=swap"
-                          rel="stylesheet"/>
-
-
-        </Head>
       <Header/>
 
             {children}
